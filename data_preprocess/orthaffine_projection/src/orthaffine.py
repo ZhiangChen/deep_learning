2f7aebad9c2f24d73f46fe8cdc4ac864774e5684
ZhiangChen <zxc251@case.edu>
2017-03-23 17:30:57 -0400

    orthaffine

--- data_preprocess/orthaffine_projection/src/orthaffine.py
+++ data_preprocess/orthaffine_projection/src/orthaffine.py
@@ -30,8 +30,9 @@ class OrthAffine():
 		self.bmY = rospy.get_param("bmY")
 		self.bmZ = rospy.get_param("bmZ")
 		self.image_size = rospy.get_param("image_size")
+		self.box_size = rospy.get_param("box_size")
 		self.bridge = CvBridge()
-		rospy.loginfo("Initialized!")
+		rospy.loginfo("Orthaffine Initialized!")
 
 	def readpcd(self,filename):
 		if filename.split('.')[1] != 'pcd':
@@ -41,19 +42,43 @@ class OrthAffine():
 			self.points = self.p.to_array()
 			return True
 
+	def readpoints(self,points):
+		self.points = points
+
 	def affine(self):
 		yz = self.points[:,1:3]
 		new_y = np.dot(yz,np.transpose(self.R))[:,0]
 		self.points[:,1] = new_y
 
 	def interpolate(self,theta):
+		'''This method is only used to interpolate large box'''
 		xy = self.points[:,0:2]
 		z = self.points[:,2]
 		x_step_size = (self.bmX - self.bnX)/self.image_size
 		y_step_size = (self.bmY - self.bnY)/self.image_size*cos(theta)
-		grid_x = np.asarray([self.bnX+x_step_size*i for i in range(self.image_size)]).reshape((-1,1))
+		delta = (self.box_size - self.image_size)/2
+		x_start = self.bnX - x_step_size*delta
+		y_start = self.bnY - y_step_size*delta
+		grid_x = np.asarray([x_start+x_step_size*i for i in range(self.box_size)]).reshape((-1,1))
+		grid_x = np.repeat(grid_x,self.box_size,axis=1)
+		grid_y = np.asarray([y_start*cos(theta)+y_step_size*i for i in range(self.box_size)]).reshape((-1,1))
+		grid_y = np.repeat(grid_y,self.box_size,axis=1).transpose()
+		grid_z = griddata(xy,z,(grid_x,grid_y),method='nearest',fill_value=0.0)
+		new_points = np.asarray([grid_x,grid_y,grid_z]).transpose().reshape((-1,3)).astype(np.float32)
+		#print(new_points.transpose().shape)
+		#new_points = np.swapaxes(new_points,0,2).reshape((-1,3)).astype(np.float32)
+		#print(new_points.shape)
+		self.points = new_points.copy()
+
+	def interpolate(self,theta,bnX,bmX,bnY,bmY):
+		'''This method is only used to interpolate small box defined by its arguments'''
+		xy = self.points[:,0:2]
+		z = self.points[:,2]
+		x_step_size = (bmX - bnX)/self.image_size
+		y_step_size = (bmY - bnY)/self.image_size*cos(theta)
+		grid_x = np.asarray([bnX+x_step_size*i for i in range(self.image_size)]).reshape((-1,1))
 		grid_x = np.repeat(grid_x,self.image_size,axis=1)
-		grid_y = np.asarray([self.bnY*cos(theta)+y_step_size*i for i in range(self.image_size)]).reshape((-1,1))
+		grid_y = np.asarray([bnY*cos(theta)+y_step_size*i for i in range(self.image_size)]).reshape((-1,1))
 		grid_y = np.repeat(grid_y,self.image_size,axis=1).transpose()
 		grid_z = griddata(xy,z,(grid_x,grid_y),method='nearest',fill_value=0.0)
 		new_points = np.asarray([grid_x,grid_y,grid_z]).transpose().reshape((-1,3)).astype(np.float32)
@@ -63,10 +88,20 @@ class OrthAffine():
 		self.points = new_points.copy()
 
 	def savepcd(self,filename):
+		'''for both small and large depending on the previous'''
 		self.p.from_array(self.points)
 		self.p._to_pcd_file(filename)
 
-	def project(self):
+	def project_large(self):
+		'''only for large box'''
+		z = self.points[:,2]
+		z_mean = (self.bmZ - self.bnZ)/2.0
+		z = (z - z_mean)/(self.bmZ - self.bnZ)
+		self.image_numpy = np.flipud(z.reshape(self.box_size,self.box_size))
+		return z
+
+	def project_small(self):
+		'''only for small box'''
 		z = self.points[:,2]
 		z_mean = (self.bmZ - self.bnZ)/2.0
 		z = (z - z_mean)/(self.bmZ - self.bnZ)
@@ -74,6 +109,7 @@ class OrthAffine():
 		return z
 		
 	def saveimage(self,filename):
+		'''saveimage can save small box image or large box image depending on what interpolate and project methods are used previously'''
 		z_mean = (self.bmZ - self.bnZ)/2.0
 		vmin = (self.bnZ - z_mean)/(self.bmZ - self.bnZ)
 		vmax = (self.bmZ - z_mean)/(self.bmZ - self.bnZ)
@@ -81,6 +117,7 @@ class OrthAffine():
 		plt.savefig(filename)
 
 	def publishimage(self):
+		'''for both small and large depending on the previous'''
 		image = ((self.image_numpy + 0.65)*255).astype(np.uint8)
 		ros_image = self.bridge.cv2_to_imgmsg(image, encoding="mono8")
 		self.pub2.publish(ros_image)
@@ -93,10 +130,9 @@ class OrthAffine():
 		self.points = np.asarray(pts)
 		self.affine()
 		self.interpolate(self.theta)
-		image_array = self.project()
+		image_array = self.project_small()
 		self.pub1.publish(image_array)
 		self.publishimage()
-		#self.saveimage('duck')
 		sys.stdout.write(".")
 		sys.stdout.flush()
 
