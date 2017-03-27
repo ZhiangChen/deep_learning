975cef61bc5dea84e0a0c10023bd3af367650a42
ZhiangChen <zxc251@case.edu>
2017-03-27 12:25:57 -0400

    cropper

--- data_preprocess/orthaffine_projection/src/cropper.py
+++ data_preprocess/orthaffine_projection/src/cropper.py
@@ -12,37 +12,81 @@ import sys
 from six.moves import cPickle as pickle
 
 class Cropper():
-	def __init__(self, keep_size):
-		self.keep_size = keep_size
+	def __init__(self):
+		self.image_size = rospy.get_param("image_size")
 		self.sub = rospy.Subscriber('/box_image/numpy', numpy_msg(Floats), self.callback, queue_size=1)
 		self.pub = rospy.Publisher('/cropped_box_image/numpy', numpy_msg(Floats), queue_size=1)
-		self.image_size = rospy.get_param("image_size")
+		self.box_size = rospy.get_param("box_size")
 		rospy.loginfo("Initialized!")
 
 	def centercrop(self,image_numpy):
-		[x,y] = [(l-self.keep_size)/2 for l in image_numpy.shape]
-		self.center_image = image_numpy[x:x+self.keep_size , y:y+self.keep_size]
+		[x,y] = [(l-self.image_size)/2 for l in image_numpy.shape]
+		self.center_image = image_numpy[x:x+self.image_size , y:y+self.image_size]
 
 	def randomcrop(self, image_numpy, nm):
-		[w,h] = [(l-self.keep_size) for l in image_numpy.shape]
+		[w,h] = [(l-self.image_size) for l in image_numpy.shape]
 		indices = list()
 		images = list()
 		while len(images)<nm:
 			[x,y] = [np.random.randint(0,l) for l in [w,h]]
 			if [x,y] not in indices:
 				indices.append([x,y])
-				images.append(image_numpy[x:x+self.keep_size , y:y+self.keep_size])
+				images.append(image_numpy[x:x+self.image_size , y:y+self.image_size])
+		images = np.asarray(images).reshape(-1,self.image_size,self.image_size)
 		return images
 
+	def set_boxes(self,bias):
+		'bias representing the bias of the center of box w.r.t the center of image is a 2D list with shape (-1,2)'
+		center = np.asarray([self.image_size/2, self.image_size/2])
+		bias = np.asarray(bias)
+		delta = np.asarray([(self.box_size - self.image_size)/2, (self.box_size - self.image_size)/2])
+		self.xy = delta + bias
+
+	def arraycrop(self,image_numpy,save_crop=False):
+		images = np.asarray([image_numpy[x:x+self.image_size , y:y+self.image_size] for x,y in self.xy]).reshape(-1,self.image_size,self.image_size)
+		return images
+
+	def crop_pickle(self,pickle_name,func=1,bias=[[0,0]],nm=5):
+		'''
+		func=1: arraycrop
+		func=2: randomcrop
+		'''
+		with open(pickle_name,'rb') as f:
+			save = pickle.load(f):
+			data = save['image']
+			del save
+		images = dict()
+		if func == 1:
+			self.set_boxes(bias)
+			for name, value in data.iteritems():
+				cropped_images = self.arraycrop(value)
+				images.setdefault(name,cropped_images)
+
+		elif func == 2:
+			for name, value in data.iteritems():
+				cropped_images = self.randomcrop(value,nm)
+				images.setdefault(name,cropped_images)
+
+		with open('new_'+pickle_name,'wb') as f:
+			save={
+			'image': images
+			}
+			pickle.dump(save,f,pickle.HIGHEST_PROTOCOL)
+			f.close()				
+
+
+
+
+
 	def callback(self, image_array):
-		image_numpy = image_array.data.reshape(self.image_size,self.image_size)
+		image_numpy = image_array.data.reshape(self.box_size,self.box_size)
 		self.centercrop(image_numpy)
 		self.pub.publish(self.center_image.reshape(-1))
 		sys.stdout.write(".")
 		sys.stdout.flush()
 
-def SaveRandomCropped(keep_size,nm):
-	cropper = Cropper(keep_size)
+def SaveRandomCropped(image_size,nm):
+	cropper = Cropper(image_size)
 	with open('depth_data_numpy', 'rb') as f:
 		save = pickle.load(f)
 		images = save['image']
